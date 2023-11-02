55d922685247ac73c2fc18759e288f001a91ca5e
donnellyfj <donnellyfj@gmail.com>
2023-11-02 00:43:46 -0400

    Service call as timer callback working

--- src/py_srvcli/py_srvcli/client_member_function.py
+++ src/py_srvcli/py_srvcli/client_member_function.py
@@ -5,13 +5,14 @@ from custom_interfaces.msg import Sphere
 import rclpy
 from rclpy.node import Node
 from rclpy.callback_groups import ReentrantCallbackGroup
+from rclpy.executors import MultiThreadedExecutor
 
 
 class MinimalClientAsync(Node):
 
     def __init__(self):
         super().__init__('minimal_client_async')
-        # cb_group = ReentrantCallbackGroup()
+        cb_group = ReentrantCallbackGroup()
         # self.did_run = self.did_get_result = False
 
         # Initialize service clients
@@ -31,10 +32,12 @@ class MinimalClientAsync(Node):
         # Initialize publisher
         self.publisher_ = self.create_publisher(Sphere, 'topic', 10)
         timer_period = 0.5  # seconds
-        # timer = self.create_timer(timer_period, self.timer_callback, callback_group=cb_group)
+        timer = self.create_timer(timer_period, self.call_service, callback_group=cb_group)
     
     def timer_callback(self):
         self.get_logger().info('%d' % self.i)
+        self.send_request(1)
+        self.send_request(2)
         self.publish_data(self.data1, 1)
         self.publish_data(self.data2, 2)
         self.i += 1
@@ -48,19 +51,52 @@ class MinimalClientAsync(Node):
             cli = self.cli2
 
         req.i = self.i
+        self.get_logger().info('here')
         self.future = cli.call_async(req)
-        rclpy.spin_until_future_complete(self, self.future)
-        self.get_logger().info('%d' % id)
-        # self.publish_data(self.future.result())
-        self.get_logger().info(
-            'Receved and published data for server %d' % id)
-        if id == 1:
-            self.data1 = self.future.result()
-            return self.data1
-        else:
-            self.data2 = self.future.result()
-            return self.data2
+        # rclpy.spin_until_future_complete(self, self.future)
+        while rclpy.ok():
+            if self.future.done() and self.future.result():
+                self.get_logger().info('%d' % id)
+                # self.publish_data(self.future.result())
+                self.get_logger().info(
+                    'Receved and published data for server %d' % id)
+                if id == 1:
+                    self.data1 = self.future.result()
+                    return self.data1
+                else:
+                    self.data2 = self.future.result()
+                    return self.data2
         # return self.future.result()
+        return None
+    
+    async def call_service(self):
+        # nonlocal cli, node, did_run, did_get_result
+        did_run = True
+        try:
+            # req = Sensor3DOF.Request()
+            # req.a = 41
+            # req.b = 1
+            future = self.cli1.call_async(self.req1)
+            result = await future
+            self.data1 = result
+            self.get_logger().info(
+                'Receved and published data for server %d' % 1)
+        finally:
+            did_get_result = True
+            self.publish_data(result, 1)
+        
+        try:
+            # req = Sensor3DOF.Request()
+            # req.a = 41
+            # req.b = 1
+            future = self.cli2.call_async(self.req2)
+            result = await future
+            self.data1 = result
+            self.get_logger().info(
+                'Receved and published data for server %d' % 2)
+        finally:
+            did_get_result = True
+            self.publish_data(result, 2)
 
     def publish_data(self, response, id):
         self.get_logger().info('here')
@@ -87,19 +123,25 @@ def main():
     # while rclpy.ok and not did_run:
     #     rclpy.spin_once(minimal_client)
 
-    response1 = minimal_client.send_request(1)
-    # # minimal_client.publish_data(response)
-    minimal_client.get_logger().info(
-        'Result of get_sensor_data: %d, %d, %d' % (response1.x, response1.y, response1.z))
+    # response1 = minimal_client.send_request(1)
+    # # # minimal_client.publish_data(response)
+    # minimal_client.get_logger().info(
+    #     'Result of get_sensor_data: %d, %d, %d' % (response1.x, response1.y, response1.z))
     
-    response2 = minimal_client.send_request(2)
-    # # minimal_client.publish_data(response)
-    minimal_client.get_logger().info(
-        'Result of get_sensor_data: %d, %d, %d' % (response2.x, response2.y, response2.z))
+    # response2 = minimal_client.send_request(2)
+    # # # minimal_client.publish_data(response)
+    # minimal_client.get_logger().info(
+    #     'Result of get_sensor_data: %d, %d, %d' % (response2.x, response2.y, response2.z))
     
-    timer_period = 0.5  # seconds
-    timer = minimal_client.create_timer(timer_period, minimal_client.timer_callback, callback_group=cb_group)
-    rclpy.spin(minimal_client)
+    # timer_period = 0.5  # seconds
+    # timer = minimal_client.create_timer(timer_period, minimal_client.timer_callback, callback_group=cb_group)
+
+    # executor = MultiThreadedExecutor()
+    # executor.add_node(minimal_client)
+    # executor.spin()
+    
+    while rclpy.ok() :
+        rclpy.spin_once(minimal_client)
 
     minimal_client.destroy_node()
     rclpy.shutdown()
